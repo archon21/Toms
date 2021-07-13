4bca446a40e42fc9784a1c1e8afc4e6d29048188
Ryan Wetmore <ryanwetmore@Ryans-MacBook-Pro.local>
2021-07-13 14:43:54 -0400

    mobx-react

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { Layout, Typography, Content } from "../components";
+import { Layout, Typography, Content, Inputs } from "../components";
 
 interface DefaultState {
   base: Array<string>;
@@ -14,44 +14,69 @@ const Home: React.FC<Props> = ({ defaultState }) => {
   // const [defaultState, setDefaultState] = useState({});
 
   return (
-    <Layout.WindoW init xAlign="flex-start" yAlign="flex-start" column>
-      <Layout.Flex margin="50px 0" column>
-        <Typography.Typography
-          color="textPrimary"
-          variant="h1"
-          textAlign="center"
-
-          // displayAlign={{}}
-        >
-          Services
-        </Typography.Typography>
-        <Layout.Flex>
-          <Content.Image src="/assets/images/services.jpeg"></Content.Image>
-          <Typography.List
-            items={[
-              "Dumpster Rentals",
-              "Deliveries",
-              "Tractor Services",
-              "Forest Maintenance",
-              "Lawn and Garden",
-              "Leaf Removal",
-            ]}
-            listItemConfig={{ typographyConfig: { variant: "h5" } }}
-          ></Typography.List>
+    <React.Fragment>
+      <Layout.WindoW init xAlign="flex-start" yAlign="flex-start" column>
+        <Layout.Flex margin="50px 0" column>
+          <Typography.Typography
+            color="textPrimary"
+            variant="h1"
+            textAlign="center"
+          >
+            Services
+          </Typography.Typography>
+          <Layout.Flex>
+            <Content.Image src="/assets/images/services.jpeg"></Content.Image>
+            <Typography.List
+              items={[
+                "Dumpster Rentals",
+                "Deliveries",
+                "Tractor Services",
+                "Forest Maintenance",
+                "Lawn and Garden",
+                "Leaf Removal",
+              ]}
+              listItemConfig={{ typographyConfig: { variant: "h5" } }}
+            ></Typography.List>
+          </Layout.Flex>
         </Layout.Flex>
+      </Layout.WindoW>
+      <Layout.WindoW init xAlign="flex-start" yAlign="flex-start" column>
+        <Layout.Flex margin="50px 0" column>
+          <Typography.Typography
+            color="textPrimary"
+            variant="h1"
+            textAlign="center"
+          >
+            Dumpster Rentals
+          </Typography.Typography>
+          <Layout.Flex>
+            <Content.Image src="/assets/images/services.jpeg"></Content.Image>
+            <Typography.List
+              items={[
+                "Dumpster Rentals",
+                "Deliveries",
+                "Tractor Services",
+                "Forest Maintenance",
+                "Lawn and Garden",
+                "Leaf Removal",
+              ]}
+              listItemConfig={{ typographyConfig: { variant: "h5" } }}
+            ></Typography.List>
+          </Layout.Flex>
 
-        {/* <Icon size="veryLarge" name="send" color="safe"></Icon> */}
+          {/* <Icon size="veryLarge" name="send" color="safe"></Icon> */}
 
-        {/* <Buttons.Button
+          {/* <Buttons.Button
           background="safe"
           color="background"
           onClick={function () {}}
         >
           Click Me
         </Buttons.Button> */}
-        {/* <Inputs.SlateEditor></Inputs.SlateEditor> */}
-      </Layout.Flex>
-    </Layout.WindoW>
+          <Inputs.SlateEditor></Inputs.SlateEditor>
+        </Layout.Flex>
+      </Layout.WindoW>
+    </React.Fragment>
   );
 };
 
